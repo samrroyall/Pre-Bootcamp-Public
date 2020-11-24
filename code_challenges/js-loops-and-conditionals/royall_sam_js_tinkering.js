6da6ccb7a2a78539707fbc2aaea66f63b211449a
Sam Royall <samrroyall@gmail.com>
2020-11-24 21:23:44 +0000

    JS Code Challenge 4 Complete

---
+++ code_challenges/js-loops-and-conditionals/royall_sam_js_tinkering.js
@@ -0,0 +1,10 @@
+var childHeight = 54;
+
+function displayIfChildIsAbleToRideTheRollerCoaster() {
+    if (childHeight > 52) {
+        console.log("Get on that ride, kiddo!");
+    } else {
+        console.log("Sorry kiddo. Maybe next year.");
+    }
+
+}
